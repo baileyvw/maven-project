c21c0e5554e9f6c017056ec55d1bfd94751bb2af
Vince Bailey <baileyvw@proweb.co.uk>
2019-06-19 12:45:10 +0100

    fix java error

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,10 +14,10 @@ public class Greeter {
 
  /**
   * @param someone
-  * @retirn Greetings
+  * @return Greetings
   */
 
-  public final String greet(final String someone) {
+  public final String greet final (String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
